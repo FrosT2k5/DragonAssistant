92150c022ff5d15d602bbb710764176cab6659a3
FrosT2k5 <androygaming@gmail.com>
2021-07-12 16:12:01 +0530

    setup: install sox and mp3 codecs

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ NC="\033[0m" # reset color
 # Install some packages
 if [[ -e "/usr/bin/apt-get" ]]; then
     echo -e "${GREEN}Installing Some requirements${NC}"
-    sudo apt-get install python3 python3-pip curl wget python3-pyaudio espeak -y
+    sudo apt-get install python3 python3-pip curl wget python3-pyaudio espeak sox libsox-fmt-all -y
 else
    echo -e "${RED}Looks like you aren't using a debian based distro, please install python, pip, python3-pyaudio and espeak with your distro's package manager${NC}"
 fi
